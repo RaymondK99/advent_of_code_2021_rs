4268ce3d883173f1044ce9b955f8a1fc6bf91f43
Raymond Kalander <raymond.kalander@getswish.se>
2021-12-14 22:27:37 +0100

    added day 14

--- src/util/day_14.rs
+++ src/util/day_14.rs
@@ -1,8 +1,9 @@
+use std::collections::{HashMap, VecDeque};
 use super::Part;
 
 pub fn solve(input : String, part: Part) -> String {
 
-    let lines:Vec<&str> = input.lines()
+    let lines:Vec<&str> = input.split("\n\n")
         .collect();
 
     match part {
@@ -12,13 +13,91 @@ pub fn solve(input : String, part: Part) -> String {
 }
 
 
-fn part1(_lines:Vec<&str>) -> String {
-    "1".to_string()
+fn parse(lines:Vec<&str>) -> (VecDeque<char>, Vec<([char;2], char)>) {
+    let mut it = lines.iter();
+    let template:VecDeque<char> = it.next().unwrap().chars().collect();
+
+    let rules:Vec<([char;2], char)> = it.next().unwrap().lines().map(|line|{
+        let mut columns = line.split(" -> ");
+        let chars = columns.next().unwrap().as_bytes();
+        let pair:[char;2] = [*chars.get(0).unwrap() as char, *chars.get(1).unwrap() as char];
+        let insertion:char = *columns.next().unwrap().as_bytes().first().unwrap() as char;
+
+        (pair.clone(), insertion)
+    }).collect();
+
+    (template, rules)
+}
+
+fn get_rule(a:char, b:char, rules:&Vec<([char;2], char)>) -> char {
+    *rules.iter().find( | (chars, _) | chars[0] == a && chars[1] == b )
+        .map(|(_,ch)| ch)
+        .unwrap()
 }
 
 
-fn part2(_lines:Vec<&str>) -> String {
-    "1".to_string()
+fn count_chars(first:char, next:char, cache:&mut HashMap<(char,char,usize),[u128;30] >, rules:&Vec<([char;2], char)>, step:usize) -> [u128;30] {
+
+    let key = (first, next, step);
+    if cache.contains_key(&key) {
+        return *cache.get(&key).unwrap();
+    }
+
+    let middle = get_rule(first, next, rules);
+
+    if step > 1 {
+        let mut count1 = count_chars(first,middle, cache,rules, step - 1);
+        let count2 = count_chars(middle,next, cache, rules, step - 1);
+
+        // Cache result
+        for i in 0..count2.len() {
+            count1[i] += count2[i];
+        }
+        cache.insert(key, count1);
+
+        return count1;
+    } else {
+        let mut count:[u128;30] = [0;30];
+        count[first as usize - 'A' as usize] += 1;
+        count[middle as usize - 'A' as usize] += 1;
+
+        return count;
+    }
+}
+
+fn calculate_polymer(lines:Vec<&str>, steps:usize) -> (u128, u128) {
+    let (mut polymer, rules) = parse(lines);
+
+    let mut count:[u128;30] = [0;30];
+    let mut cache = HashMap::new();
+
+    for _ in 0..polymer.len() - 1 {
+        let first  = polymer.pop_front().unwrap();
+        let second = *polymer.front().unwrap();
+        let c = count_chars(first, second, &mut cache, &rules,  steps );
+        for i in 0..c.len() {
+            count[i] += c[i];
+        }
+    }
+
+    // Add last char
+    let last_char = *polymer.back().unwrap();
+    count[last_char as usize - 'A' as usize] += 1;
+
+    let min = *count.iter().filter(|&n| *n > 0).min().unwrap();
+    let max = *count.iter().max().unwrap();
+
+    (max, min)
+}
+
+fn part1(lines:Vec<&str>) -> String {
+    let (max, min) = calculate_polymer(lines, 10);
+    (max - min).to_string()
+}
+
+fn part2(lines:Vec<&str>) -> String {
+   let (max, min) = calculate_polymer(lines, 40);
+    (max - min).to_string()
 }
 
 
@@ -32,30 +111,64 @@ mod tests {
     #[test]
     fn test1() {
 
-        let input = "";
+        let input = "NNCB
 
-        assert_eq!("1", solve(input.to_string(), Part1));
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+
+        assert_eq!("1588", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test_part1() {
         let input = include_str!("../../input_14.txt");
 
-        assert_eq!("1", solve(input.to_string(), Part1));
+        assert_eq!("2712", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test2() {
 
-        let input = "";
-        assert_eq!("1", solve(input.to_string(), Part2));
+        let input = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+        assert_eq!("2188189693529", solve(input.to_string(), Part2));
     }
 
     #[test]
     fn test_part2() {
         let input = include_str!("../../input_14.txt");
 
-        assert_eq!("1", solve(input.to_string(), Part2));
+        assert_eq!("8336623059567", solve(input.to_string(), Part2));
     }
 
 }
