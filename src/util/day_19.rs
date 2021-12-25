4af027bb6f664cda67ad5a03b735e276ca19dda8
Raymond Kalander <raymond.kalander@getswish.se>
2021-12-25 09:48:42 +0100

    added more days

--- src/util/day_19.rs
+++ src/util/day_19.rs
@@ -1,8 +1,9 @@
+use permute::permute;
 use super::Part;
 
 pub fn solve(input : String, part: Part) -> String {
 
-    let lines:Vec<&str> = input.lines()
+    let lines:Vec<&str> = input.split("\n\n")
         .collect();
 
     match part {
@@ -11,8 +12,110 @@ pub fn solve(input : String, part: Part) -> String {
     }
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+struct Pos {
+    x:i32,
+    y:i32,
+    z:i32,
+}
 
-fn part1(_lines:Vec<&str>) -> String {
+#[derive(Debug)]
+struct Scanner {
+    number:usize,
+    beacons:Vec<Pos>,
+    permutation:usize,
+}
+
+
+impl Pos {
+
+    fn get_permutations(&self) -> Vec<Pos>{
+        let initial_state = vec![self.x, self.y, self.z];
+        let states = permute(initial_state);
+        let directions: Vec<(i32, i32, i32)> = vec![(1, 1, 1), (1, 1, -1), (1, -1, 1), (1, -1, -1), (-1, 1, 1), (-1, 1, -1), (-1, -1, 1), (-1, -1, -1)];
+
+        let mut permutations = vec![];
+        for state in states.iter().map(|s| s.as_slice()) {
+            for i in 0..directions.len() {
+                let (x,y,z) = directions.get(i).unwrap();
+                permutations.push(Pos{x:*x*state[0], y:*y*state[1], z:*z*state[2]})
+            }
+        }
+
+        permutations
+    }
+
+    fn _get_permutation(&self,variant:usize) -> Pos {
+        let v = self.get_permutations();
+        *v.get(variant).unwrap()
+    }
+
+    fn _get_scanner_candidate_pos(&self, other:&Pos) -> Pos {
+        let x = self.x - other.x;
+        let y = self.y - other.y;
+        let z = self.z - other.z;
+        Pos{x,y,z}
+    }
+
+}
+impl Scanner {
+
+    fn new(lines:&str, number:usize) -> Scanner {
+        let mut it = lines.lines();
+        let mut beacons = vec![];
+        it.next();
+        loop {
+            if let Some(line) = it.next() {
+                let mut cols = line.split(',');
+                let x = cols.next().unwrap().parse::<i32>().unwrap();
+                let y = cols.next().unwrap().parse::<i32>().unwrap();
+                let z = cols.next().unwrap().parse::<i32>().unwrap();
+                beacons.push(Pos {x,y,z});
+            } else {
+                break;
+            }
+        }
+
+        Scanner{number, beacons,permutation:0}
+    }
+
+    fn _next(&mut self) {
+        self.permutation = self.permutation + 1;
+    }
+
+    fn _get_pos(&self, index:usize) -> Pos {
+        self.beacons.get(index).unwrap()._get_permutation(self.permutation)
+    }
+
+}
+
+
+
+fn match_scanners(_scanner0:&Scanner, _scanner:&mut Scanner) {
+    /*
+    for pos_scanner0 in scanner0.beacons.iter() {
+        for i in 0..24 {
+            for index in 0..scanner.beacons.len() {
+                let pos = scanner.get_pos(index);
+                println!("pos no {}:{:?}", index, pos);
+
+            }
+            scanner.next();
+
+        }
+
+    }
+
+     */
+    //println!("{:?}", permutations);
+}
+
+fn part1(lines:Vec<&str>) -> String {
+
+    let mut scanners:Vec<Scanner> = lines.iter().enumerate().map(|(i,elem)| Scanner::new(elem, i) ).collect();
+
+    match_scanners(&scanners.remove(0), scanners.get_mut(1).unwrap());
+    //println!("{:?}", scanners.first().unwrap());
     "1".to_string()
 }
 
@@ -24,15 +127,70 @@ fn part2(_lines:Vec<&str>) -> String {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
     use util::Part::{Part1, Part2};
 
 
+
     #[test]
+    fn test91() {
+        let pos = Pos{x:1,y:2,z:3};
+
+        let p = pos.get_permutations();
+        println!("{}, {:?}", p.len(), p);
+        let mut s = HashSet::new();
+        for n in p {
+            s.insert(n);
+        }
+
+        println!("{}, {:?}", s.len(), s);
+
+
+    }
+        #[test]
     fn test1() {
 
-        let input = "";
+        let input = "--- scanner 0 ---
+-1,-1,1
+-2,-2,2
+-3,-3,3
+-2,-3,1
+5,6,-4
+8,0,7
+
+--- scanner 0 ---
+1,-1,1
+2,-2,2
+3,-3,3
+2,-1,3
+-5,4,-6
+-8,-7,0
+
+--- scanner 0 ---
+-1,-1,-1
+-2,-2,-2
+-3,-3,-3
+-1,-3,-2
+4,6,5
+-7,0,8
+
+--- scanner 0 ---
+1,1,-1
+2,2,-2
+3,3,-3
+1,3,-2
+-4,-6,5
+7,0,8
+
+--- scanner 0 ---
+1,1,1
+2,2,2
+3,3,3
+3,1,2
+-6,-4,-5
+0,7,-8";
 
         assert_eq!("1", solve(input.to_string(), Part1));
     }
--- src/util/day_21.rs
+++ src/util/day_21.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use super::Part;
 
 pub fn solve(input : String, part: Part) -> String {
@@ -12,13 +13,175 @@ pub fn solve(input : String, part: Part) -> String {
 }
 
 
-fn part1(_lines:Vec<&str>) -> String {
-    "1".to_string()
+#[derive(Debug)]
+struct Player {
+    number:u64,
+    position:u64,
+    score:u64,
 }
 
 
-fn part2(_lines:Vec<&str>) -> String {
-    "1".to_string()
+impl Player {
+
+    fn parse(line:&str) -> Player {
+        //Player 1 starting position: 4
+        let re = Regex::new(r"Player (\d*) starting position: (\d*)").unwrap();
+        for cap in re.captures_iter(line) {
+            let (number, position) =
+                (cap[1].parse::<u64>().unwrap(), cap[2].parse::<u64>().unwrap());
+            return Player{number, position, score:0}
+        }
+        panic!("...");
+    }
+
+    fn roll(&mut self, dice:&mut u64, rolls:&mut u64) -> bool {
+        for _ in 0..3 {
+            self.advance(*dice+1);
+            *dice = (*dice + 1) % 100;
+        }
+
+        self.score += self.position;
+        *rolls +=3;
+
+        self.score >= 1000
+    }
+
+    fn advance(&mut self, dice_value:u64) {
+        self.position += dice_value;
+        while self.position > 10 {
+            self.position -= 10;
+        }
+    }
+}
+
+
+
+
+fn part1(lines:Vec<&str>) -> String {
+    let mut game_context:Vec<Player> = lines.iter().map( |line| Player::parse(line)).collect();
+    let mut player2 = game_context.pop().unwrap();
+    let mut player1 = game_context.pop().unwrap();
+
+    let mut rolls = 0;
+    let mut dice = 0;
+    loop {
+        if player1.roll(&mut dice, &mut rolls) {
+            // Player 1 wins
+            println!("player 2 score:{}, rolls={}",player2.score, rolls);
+            return (player2.score * rolls).to_string()
+        }
+        if player2.roll(&mut dice, &mut rolls) {
+            println!("player 1 score:{}, rolls={}",player1.score, rolls);
+            return (player1.score * rolls).to_string()
+        }
+    }
+}
+
+fn get_number_of_outcomes(dice_value:u64) -> u64 {
+    return match dice_value {
+        3 => 1,
+        4 => 3,
+        5 => 6,
+        6 => 7,
+        7 => 6,
+        8 => 3,
+        9 => 1,
+        _ => panic!("..."),
+    }
+}
+
+fn play_part2(state:&mut Vec<Vec<Option<(u64, u64)>>>, player1_score:u64, player1_pos:u64, player2_score:u64, player2_pos:u64, player1_turn:bool) -> (u64, u64){
+    let mut player1_acc_wins:u64 = 0;
+    let mut player2_acc_wins:u64 = 0;
+
+    // Check if we already have the count for this combo
+    let pos_index = ((player1_pos << 5) + player2_pos) as usize;
+    let points_index = ((player1_score << 5) + player2_score) as usize;
+
+    //println!("play => player1:{},{}, player2:{},{}", player1_score, player1_pos, player2_score, player2_pos);
+    if let Some(state_element) = state.get(pos_index).unwrap().get(points_index).unwrap() {
+        //println!("    => Cached res {:?}", state_element);
+        return *state_element;
+    }
+
+
+    // Check if any player reached 21?
+    if player1_score > 20 {
+        // Player 1 wins...
+        player1_acc_wins += 1;
+    } else if player2_score > 20 {
+        // Player 2 wins...
+        player2_acc_wins += 1;
+    } else {
+        // Roll dices
+        let rolls: Vec<(u64, u64)> = (3..=9).into_iter().map(|v| (v, get_number_of_outcomes(v))).collect();
+
+        for (dice_value, outcomes) in rolls {
+            if player1_turn {
+                let mut player1_next_pos = player1_pos + dice_value;
+                while player1_next_pos > 10 {
+                    player1_next_pos -= 10;
+                }
+                let player1_next_score = player1_score + player1_next_pos;
+
+                // Call next game
+                let (player1_wins, player2_wins) = play_part2(state, player1_next_score, player1_next_pos, player2_score, player2_pos, !player1_turn);
+
+                // Accumulate number of outcomes for this branch
+                player1_acc_wins += player1_wins * outcomes;
+                player2_acc_wins += player2_wins * outcomes;
+
+            } else {
+                let mut player2_next_pos = player2_pos + dice_value;
+                while player2_next_pos > 10 {
+                    player2_next_pos -= 10;
+                }
+                let player2_next_score = player2_score + player2_next_pos;
+
+                // Call next game
+                let (player1_wins, player2_wins) = play_part2(state, player1_score, player1_pos, player2_next_score, player2_next_pos, !player1_turn);
+
+                // Accumulate number of outcomes for this branch
+                player1_acc_wins += player1_wins * outcomes;
+                player2_acc_wins += player2_wins * outcomes;
+
+            }
+        }
+    }
+
+    // Update cached state
+    let update_state_element = state.get_mut(pos_index).unwrap().get_mut(points_index).unwrap();
+    *update_state_element = Some((player1_acc_wins, player2_acc_wins));
+
+    //println!("    => CALCULATE res {:?}", update_state_element);
+
+    return (player1_acc_wins, player2_acc_wins)
+}
+
+fn part2(lines:Vec<&str>) -> String {
+    let mut game_context:Vec<Player> = lines.iter().map( |line| Player::parse(line)).collect();
+    println!("{:?}",game_context);
+    let player2 = game_context.pop().unwrap();
+    let player1 = game_context.pop().unwrap();
+
+    let mut state = vec![];
+    for _ in 0..1024 {
+        let mut sub_vec = vec![];
+        for _ in 0..1024 {
+            sub_vec.push(Option::None);
+        }
+        state.push(sub_vec);
+    }
+
+    let outcomes = play_part2(&mut state, 0, player1.position, 20, player2.position, true);
+
+    //307571216519574
+    //89974745806771
+
+    //444356092776315
+    //341960390180808
+    println!("{:?}", outcomes);
+    outcomes.0.to_string()
 }
 
 
@@ -32,26 +195,28 @@ mod tests {
     #[test]
     fn test1() {
 
-        let input = "";
+        let input = "Player 1 starting position: 4
+Player 2 starting position: 8";
 
-        assert_eq!("1", solve(input.to_string(), Part1));
+        assert_eq!("739785", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test_part1() {
         let input = include_str!("../../input_21.txt");
 
-        assert_eq!("1", solve(input.to_string(), Part1));
+        assert_eq!("675024", solve(input.to_string(), Part1));
     }
 
-    #[test]
+    //#[test]
     fn test2() {
 
-        let input = "";
-        assert_eq!("1", solve(input.to_string(), Part2));
+        let input = "Player 1 starting position: 4
+Player 2 starting position: 8";
+        assert_eq!("444356092776315", solve(input.to_string(), Part2));
     }
 
-    #[test]
+    //#[test]
     fn test_part2() {
         let input = include_str!("../../input_21.txt");
 
